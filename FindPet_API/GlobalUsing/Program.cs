61bb9ec5bddd4c55017702377d920d5e161a27dd
Андрій Халін <andrii.khalin@nure.ua>
2024-05-27 17:55:38 +0300

    c

--- FindPet_API/GlobalUsing/Program.cs
+++ FindPet_API/GlobalUsing/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿global using FindPet.Domain.Entities;
+global using FindPet.Domain.DTOs;
+global using Microsoft.AspNetCore.Http;
+global using FindPet.Infrastructure.Interfaces;
+global using FindPet.Infrastructure.Data;
+global using FindPet.Core.Services;
+global using FindPet.Core.Repositories;

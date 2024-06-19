2d8043064b6e2ff048e0cf462355964115905cd3
Андрій Халін <andrii.khalin@nure.ua>
2024-06-20 01:00:39 +0300

    c

---
+++ FindPet_UI/src/app/interfaces/register-request.ts
@@ -0,0 +1,9 @@
+export interface RegisterRequest {
+  name: string
+  email: string
+  password: string
+  birthDate: string
+  phoneNumber: string
+  photo: string
+  roles: string[]
+}
--- FindPet_UI/src/app/pages/register/register.component.ts
+++ FindPet_UI/src/app/pages/register/register.component.ts
@@ -1,29 +1,49 @@
 import { Component, OnInit, ViewChild, ElementRef, inject } from '@angular/core';
-import { RouterOutlet, RouterLink} from "@angular/router";
+import { RouterOutlet, RouterLink, Router} from "@angular/router";
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
-import { FormsModule, FormGroup, FormControl,FormBuilder, Validators, ReactiveFormsModule} from "@angular/forms";
-import {NgClass,NgFor,NgIf} from "@angular/common";
+import { FormsModule, FormGroup, FormControl,FormBuilder, Validators, ReactiveFormsModule, AbstractControl} from "@angular/forms";
+import {NgClass,NgFor,NgIf,AsyncPipe} from "@angular/common";
 import intlTelInput from 'intl-tel-input';
+import { RoleService } from '../../services/role.service';
+import { AuthService } from '../../services/auth.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Role } from '../../interfaces/role';
+import { Observable } from 'rxjs';
+import { ValidationError } from '../../interfaces/validation-error';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [FormsModule,RouterOutlet, RouterLink,MatIconModule,MatInputModule,NgClass,ReactiveFormsModule,NgFor,NgIf],
+  imports: [FormsModule,RouterOutlet,AsyncPipe, RouterLink,MatIconModule,MatInputModule,NgClass,ReactiveFormsModule,NgFor,NgIf],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent implements OnInit{
-showPassword: boolean = false;
+  roleService = inject(RoleService);
+  authService = inject(AuthService);
+  matSnackbar = inject(MatSnackBar);
+  roles$!: Observable<Role[]>;
+  showPassword: boolean = false;
   form : FormGroup;
+  router = inject(Router);
+  errors!: ValidationError[];
 
   constructor(private formBuilder: FormBuilder){
       this.form = formBuilder.group({
-          "fullName": ["", [Validators.required]],
+          "name": ["", [Validators.required]],
           "email": ["", [ Validators.required, Validators.email]],
           "password": ['', [Validators.required]],
-          "userPhone":[ "", [Validators.required]]
-      });
+          "confirmPassword" : ["",[Validators.required]],
+          "phoneNumber":[ "", [Validators.required]],
+          "roles": [''],
+          "photo":['',[Validators.required]]
+      },
+      {
+        validator: this.passwordMatchValidator,
+      }
+    );
   }
 
 togglePassword(): void {
@@ -39,6 +59,47 @@ ngOnInit() {
     utilsScript: "https://cdn.jsdelivr.net/npm/intl-tel-input@23.0.12/build/js/utils.js",
     separateDialCode:true
   });
+
+  this.roles$ = this.roleService.getRoles();
 }
+
+register() {
+  this.authService.register(this.form.value).subscribe(
+    {
+      next: (response) => {
+        console.log(response);
+
+        this.matSnackbar.open(response.message, 'Close', {
+          duration: 5000,
+          horizontalPosition: 'center',
+        });
+        this.router.navigate(['/login']);
+      },
+      error: (err: HttpErrorResponse) => {
+        if (err!.status === 400) {
+          this.errors = err!.error;
+          this.matSnackbar.open('Validations error', 'Close', {
+            duration: 5000,
+            horizontalPosition: 'center',
+          });
+        }
+      },
+      complete: () => console.log('Register success'),
+    }
+  );
+  }
+
+private passwordMatchValidator(
+    control: AbstractControl
+  ): { [key: string]: boolean } | null {
+    const password = control.get('password')?.value;
+    const confirmPassword = control.get('confirmPassword')?.value;
+
+    if (password !== confirmPassword) {
+      return { passwordMismatch: true };
+    }
+
+    return null;
+  }
 
 }
